81beb67888976be35456c2b7941f3bc5f3b444e2
Jake Massoth <jake@owlin.com>
2023-03-08 11:18:00 +0100

    add tmux and nvim

--- home.nix
+++ home.nix
@@ -14,7 +14,69 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
+  fonts.fontconfig.enable = true;
+  home.packages = [
+    (pkgs.nerdfonts.override { fonts = [ "Hack" ]; })
+  ];
   home.stateVersion = "22.11";
+  # packages to install
+  xdg.configFile.nvim = {
+	source = ./nvim;
+	recursive = true;
+  };
+
+  # TODO have nix manage plugins
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    defaultEditor = true;
+    extraLuaConfig = builtins.readFile ./nvim/init.lua;
+  };
+
+  # TODO have nix manage plugins
+  programs.tmux = {
+    enable = true;
+
+    keyMode = "vi";
+    mouse = true;
+    prefix = "C-a";
+
+    terminal = "screen-256color";
+
+    shell = "${pkgs.zsh}/bin/zsh";
+    extraConfig = ''
+        unbind %
+        bind | split-window -h
+
+        unbind '"'
+        bind - split-window -v
+
+
+        bind-key -T copy-mode-vi 'v' send -X begin-selection # start selecting text with "v"
+        bind-key -T copy-mode-vi 'y' send -X copy-selection # copy text with "y"
+
+        unbind -T copy-mode-vi MouseDragEnd1Pane # don't exit copy mode after dragging with mouse
+    '';
+    plugins = [
+        { 
+            plugin = pkgs.tmuxPlugins.continuum; 
+            extraConfig = "set -g @continuum-restore 'on'";
+        } 
+        {
+          plugin = pkgs.tmuxPlugins.resurrect;
+          extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+        }
+        pkgs.tmuxPlugins.vim-tmux-navigator
+        pkgs.tmuxPlugins.sensible
+        {
+            plugin = pkgs.tmuxPlugins.power-theme;
+            extraConfig = "set -g @tmux_power_theme 'defaut'";
+
+        }
+    ];
+  };
+
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
---
+++ nvim/lua/jake/core/keymaps.lua
@@ -0,0 +1,30 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+keymap.set("n", "x", '"_x"') -- don't copy into the register when deleting with x
+
+keymap.set("n", "<leader>+", "<C-a>")
+keymap.set("n", "<leader>-", "<C-x>")
+
+-- splitting windows
+keymap.set("n", "<leader>sv", "<C-w>v") -- split vertially
+keymap.set("n", "<leader>sh", "<C-w>s") -- split horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make windows equal width
+keymap.set("n", "<leader>sx", ":close<CR>")
+
+-- plugin keymaps
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle maximising a split window
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope lsp_document_symbols<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
