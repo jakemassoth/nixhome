7eebd9f20e8640cfc8cc14830a5ee6b878bd770a
Jake Massoth <jake@owlin.com>
2023-04-05 21:08:32 +0000

    add svelte to things to format with prettier

--- nvim/lua/jake/plugins/lsp/null-ls.lua
+++ nvim/lua/jake/plugins/lsp/null-ls.lua
@@ -14,7 +14,9 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
 	sources = {
-		formatting.prettier,
+		formatting.prettier.with({
+			extra_filetypes = { "svelte" },
+		}),
 		formatting.stylua,
 		formatting.gofumpt,
 		formatting.nixfmt,
