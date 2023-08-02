0d0ca596e71268c890228af3f58e1af60e5b2954
Jake Massoth <jake@owlin.com>
2023-08-02 09:36:19 +0200

    remove old machines + add NVM to zshconfig

--- devbox-jake-v5/home.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, lib, ... }:
-
-{
-  imports = [ ../home/common.nix ];
-  home.username = "ubuntu";
-  home.homeDirectory = "/home/ubuntu";
-
-  programs.home-manager.enable = true;
-}
