b5a9024765acef613848c18cb6acdf4c414d3700
Jake Massoth <jakemassoth@storyteq.com>
2024-03-13 21:16:13 +0100

    somewhat working waybar

--- home/programs/de/hyprland.nix
+++ home/programs/de/hyprland.nix
@@ -21,6 +21,61 @@ in {
     };
   };
 
+  programs.waybar = {
+    enable = true;
+    systemd.enable = true;
+    style = builtins.readFile "${pkgs.waybar}/etc/xdg/waybar/style.css";
+    settings = [{
+      # height = 30;
+      # layer = "top";
+      # position = "bottom";
+      # tray = { spacing = 10; };
+      modules-center = [ "hyprland/window" ];
+      modules-left = [ "hyprland/workspaces" "sway/mode" ];
+      modules-right = [
+        "idle_inhibitor"
+        "pulseaudio"
+        "cpu"
+        "memory"
+        "temperature"
+        "clock"
+        "tray"
+      ];
+      clock = {
+        format-alt = "{:%Y-%m-%d}";
+        tooltip-format = "{:%Y-%m-%d | %H:%M}";
+      };
+      cpu = {
+        format = "{usage}% ";
+        tooltip = false;
+      };
+      memory = { format = "{}% "; };
+      pulseaudio = {
+        format = "{volume}% {icon} {format_source}";
+        format-bluetooth = "{volume}% {icon} {format_source}";
+        format-bluetooth-muted = " {icon} {format_source}";
+        format-icons = {
+          car = "";
+          default = [ "" "" "" ];
+          handsfree = "";
+          headphones = "";
+          headset = "";
+          phone = "";
+          portable = "";
+        };
+        format-muted = " {format_source}";
+        format-source = "{volume}% ";
+        format-source-muted = "";
+        on-click = "pavucontrol";
+      };
+      temperature = {
+        critical-threshold = 80;
+        format = "{temperatureC}°C {icon}";
+        format-icons = [ "" "" "" ];
+      };
+    }];
+  };
+
   wayland.windowManager.hyprland = {
     enable = true;
     systemd.enable = true;
