44f950c09202db3a38a2d8a50eed473df36578a8
Jake Massoth <jakemassoth@storyteq.com>
2024-10-08 14:12:16 +0200

    Add zet script + flake update

--- home/common.nix
+++ home/common.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   catppuccin.enable = true;
   catppuccin.flavor = "mocha";
   fonts.fontconfig.enable = true;
@@ -7,6 +7,58 @@
     pkgs.eza
     pkgs.ripgrep
     pkgs.devbox
+    (pkgs.writeShellScriptBin "zet" ''
+      # function to prompt the user for a filename
+      get_filename() {
+      	read -p "Enter a filename: " filename
+      	echo "$filename"
+      }
+
+      # function to create and open a file in the specified directory
+      open_file() {
+
+      	# Cd into the directory
+      	cd "$1" || exit
+      	# Create the file in the specified directory
+      	touch "$1/$filename.md"
+
+      	# create unique identifier and links section
+      	timestamp="$(date +"%Y%m%d%H%M")"
+
+      	# format the file
+      	{
+      		echo "# "
+      		echo -en "\n"
+      		echo -en "\n"
+      		echo -en "\n"
+      		echo "Links:"
+      		echo -en "\n"
+      		echo "$timestamp"
+      	} >>"$1/$filename.md"
+
+      	# Open the file in Neovim
+      	nvim '+ normal ggzzi' "$1/$filename.md"
+      }
+
+      # Prompt the user if no filename is provided
+      if [[ $# -eq 0 ]]; then
+      	filename=$(get_filename)
+      fi
+
+      # if more than one argument is given, print error message and stop script
+      if [[ $# -gt 1 ]]; then
+      	echo "Please provide only one filename separated by dashes, without .md extension."
+      	echo "Example: zet my-new-note"
+      	exit 1
+      fi
+
+      # set filename to the argument given to the script
+      if [[ $# -eq 1 ]]; then
+      	filename=$1
+      fi
+
+      open_file "${config.home.homeDirectory}/Documents/jake_vault/00-inbox"
+    '')
   ];
 
   programs.tmux = {
@@ -70,6 +122,7 @@
       api = "cd ~/development/storyteq/storyteq-api";
       platform = "cd ~/development/storyteq/storyteq-platform";
       lg = "lazygit";
+      give_me_new_token = "~/give_me_new_token";
     };
     enableCompletion = true;
     syntaxHighlighting.enable = true;
