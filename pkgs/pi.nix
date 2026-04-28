94e3c0bc1c161de0ec36f13d9101bda2ee106852
Jake Massoth <jake@massoth.tech>
2026-04-28 10:25:35 +0200

    fix skills hashes

--- pkgs/pi.nix
+++ pkgs/pi.nix
@@ -1,36 +1,38 @@
-{ pkgs, pi }:
-
-let
-  buildPiLib = import ../lib/buildPi.nix { inherit pkgs; };
+{
+  pkgs,
+  pi,
+}: let
+  buildPiLib = import ../lib/buildPi.nix {inherit pkgs;};
   inherit (buildPiLib) buildPi buildPiExtension buildPiSkill;
 in
+  buildPi {
+    inherit pi;
 
-buildPi {
-  inherit pi;
+    extensions = [
+      (buildPiExtension {
+        name = "pi-vertex";
+        version = "1.1.4";
+        src = ../home/programs/pi/extensions/pi-vertex;
+        npmDepsHash = "sha256-oSsTImt76iaKEEMECb7qYS/IO54bna3EDFCLCMoLrtY=";
+      })
+    ];
 
-  extensions = [
-    (buildPiExtension {
-      name = "pi-vertex";
-      version = "1.1.4";
-      src = ../home/programs/pi/extensions/pi-vertex;
-      npmDepsHash = "sha256-oSsTImt76iaKEEMECb7qYS/IO54bna3EDFCLCMoLrtY=";
-    })
-  ];
+    skills = [
+      (buildPiSkill {
+        name = "grill-me";
+        src =
+          pkgs.fetchFromGitHub {
+            owner = "mattpocock";
+            repo = "skills";
+            rev = "383b6a06d59c4ce0ffcb14112bfd91265a86cf91";
+            hash = "sha256-zeXdZQEpMfFjzSL/yrRYJZC2aOBvlY8xE3Ol4GMGyJI=";
+          }
+          + "/skills/grill-me";
+      })
+    ];
 
-  skills = [
-    (buildPiSkill {
-      name = "grill-me";
-      src = pkgs.fetchFromGitHub {
-        owner = "mattpocock";
-        repo = "skills";
-        rev = "main";
-        hash = "sha256-+6C7uTbdCyGz/VjU15zqALzRrrWI2/0I3mhEmwdUHDg=";
-      } + "/grill-me";
-    })
-  ];
-
-  env = {
-    GOOGLE_CLOUD_PROJECT = "jake-index-demo";
-    GOOGLE_CLOUD_LOCATION = "global";
-  };
-}
+    env = {
+      GOOGLE_CLOUD_PROJECT = "jake-index-demo";
+      GOOGLE_CLOUD_LOCATION = "global";
+    };
+  }
