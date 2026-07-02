0a6c99de8bb586f9aafb8a5235d694b841ed285f
Jake Massoth <jake@massoth.tech>
2026-07-02 14:11:27 +0200

    disable vue ls

--- home/programs/neovim/lua/config.lua
+++ home/programs/neovim/lua/config.lua
@@ -89,7 +89,7 @@ vim.lsp.enable({
 	"lua_ls",
 	"elixirls",
 	"terraformls",
-	"vue_ls",
+	-- "vue_ls",
 	"ts_ls",
 	"helm_ls",
 	"marksman",
@@ -107,24 +107,24 @@ vim.lsp.enable({
 
 -- This is set by nix, we concat the two files together
 -- local vue_language_server_path = '/path/to/@vue/language-server'
-local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
-local vue_plugin = {
-	name = "@vue/typescript-plugin",
-	location = vue_language_server_path,
-	languages = { "vue" },
-	configNamespace = "typescript",
-}
-
-local ts_ls_config = {
-	init_options = {
-		plugins = {
-			vue_plugin,
-		},
-	},
-	filetypes = tsserver_filetypes,
-}
-
-vim.lsp.config("ts_ls", ts_ls_config)
+-- local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+-- local vue_plugin = {
+-- 	name = "@vue/typescript-plugin",
+-- 	location = vue_language_server_path,
+-- 	languages = { "vue" },
+-- 	configNamespace = "typescript",
+-- }
+--
+-- local ts_ls_config = {
+-- 	init_options = {
+-- 		plugins = {
+-- 			vue_plugin,
+-- 		},
+-- 	},
+-- 	filetypes = tsserver_filetypes,
+-- }
+--
+-- vim.lsp.config("ts_ls", ts_ls_config)
 
 -- configure lua server (with special settings)
 vim.lsp.config("lua_ls", {
